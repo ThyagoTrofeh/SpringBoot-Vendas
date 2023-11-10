c8054a13c6c050afa51829ee0b5f89824e7220c7
Thyago Monteiro <thyago.monteiro@liferay.com>
2023-11-10 15:45:28 -0300

    Aula 4, Criando um projeto SpringBoot do zero

---
+++ src/main/java/io/github/thyagotrofeh/VendasApllication.java
@@ -0,0 +1,12 @@
+package io.github.thyagotrofeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasApllication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApllication.class, args);
+    }
+}
