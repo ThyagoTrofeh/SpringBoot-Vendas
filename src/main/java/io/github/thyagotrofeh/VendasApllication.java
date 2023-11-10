b5ad6b19ef9312634f6313f266169b89880ad400
Thyago Monteiro <thyago.monteiro@liferay.com>
2023-11-10 16:04:46 -0300

    Aula 6 - Hello Word com Spring Boot

--- src/main/java/io/github/thyagotrofeh/VendasApllication.java
+++ src/main/java/io/github/thyagotrofeh/VendasApllication.java
@@ -2,10 +2,17 @@ package io.github.thyagotrofeh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApllication {
 
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "hello world";
+    }
     public static void main(String[] args) {
         SpringApplication.run(VendasApllication.class, args);
     }
